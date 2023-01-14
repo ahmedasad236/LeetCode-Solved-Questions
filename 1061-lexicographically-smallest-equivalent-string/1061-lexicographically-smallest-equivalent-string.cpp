0df1468d421d2cdedbaa52b20604c1356edd9f1a
ahmedasad236 <68563546+ahmedasad236@users.noreply.github.com>
2023-01-14 23:21:39 +0200

    Time: 43 ms (6.42%), Space: 6.6 MB (61.47%) - LeetHub

---
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+    int parents[26] = {0};
+    int get(int a) {
+        return parents[a] = (a == parents[a] ? a : get(parents[a]));
+    }
+    void unionSets(int a, int b) {
+        a = get(a);
+        b = get(b);
+        if(a < b)
+            parents[b] = a;
+        else parents[a] = b;
+        cout << "union of " << (char)(a+'a') << ", " << (char)(b+'a') << " is (" << (char) ( parents[a] + 'a') << ", " << (char)(parents[b] + 'a') << ")\n"; 
+    }
+    string smallestEquivalentString(string s1, string s2, string baseStr) {
+        for(int i = 0; i < 26; i++) parents[i] = i;
+        for(int i = 0; i < s2.size(); i++) {
+            unionSets(s1[i] - 'a', s2[i] - 'a');
+        }
+        
+        string res = "";
+        for(auto c : parents) {
+            cout << (char)(c + 'a') << '\n';
+        }
+        for(auto c : baseStr) {
+            res += (char)(get(c - 'a') + 'a');
+        }
+        return res;
+    }
+};
