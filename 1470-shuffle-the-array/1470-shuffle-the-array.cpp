0c4d4413e4716162965c3d64ab9374d51dfbd5d4
ahmedasad236 <68563546+ahmedasad236@users.noreply.github.com>
2023-02-06 15:07:04 +0200

    Time: 11 ms (28.61%), Space: 9.8 MB (31.98%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -3,7 +3,8 @@ public:
     vector<int> shuffle(vector<int>& nums, int n) {
         int i = 0, j = n;
         vector<int> res;
-        while(j < 2 * n) {
+        int size = 2 * n;
+        while(j < size) {
             res.push_back(nums[i++]);
             res.push_back(nums[j++]);
         }
